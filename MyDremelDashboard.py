b25067e311a219fcdacaf54cd948ef5744e08908
Ananya Saini <ananyasaini1544@gmail.com>
2026-06-03 14:05:38 +0530

    increase background opacity

--- MyDremelDashboard.py
+++ MyDremelDashboard.py
@@ -90,7 +90,7 @@ html, body, .stApp {
     background-size: cover;
     background-position: center;
     background-attachment: fixed;
-    opacity: 0.15;
+    opacity: 0.20;
     z-index: 0;
     pointer-events: none;
 }
