e8dc0b20350d9954b6d00e3a3cb693185de73909
Ananya Saini <ananyasaini1544@gmail.com>
2026-06-03 11:35:19 +0530

    fix gt variable conflict

--- MyDremelDashboard.py
+++ MyDremelDashboard.py
@@ -1152,8 +1152,8 @@ with tab2:
     td=[]; gp2=st.progress(0); kf=selected_keywords[:6]
     for i,kw in enumerate(kf):
         gp2.progress((i+1)/len(kf))
-        gt=get_trends(kw)
-        td.append({"Keyword":clean_label(kw),"Search Score":gt["current"],"Growth %":gt["growth"],"Peak":gt["peak"]})
+        gt_result=get_trends(kw)
+        td.append({"Keyword":clean_label(kw),"Search Score":gt_result["current"],"Growth %":gt_result["growth"],"Peak":gt_result["peak"]})
         time.sleep(3)
     gp2.empty()
     gt_df = pd.DataFrame(td).sort_values("Growth %", ascending=False)
